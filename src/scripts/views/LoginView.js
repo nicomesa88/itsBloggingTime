5d418298ca0843852f4696b5457ac74243ad8d57
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-07-18 09:22:36 -0500

    workign to get loging working

--- src/scripts/views/LoginView.js
+++ src/scripts/views/LoginView.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ACTIONS from '../actions'
 import Header from './header'
 
 const LoginView = React.createClass ({
@@ -6,22 +7,42 @@ const LoginView = React.createClass ({
         return (
             <div className = 'loginView'>
                 <Header />
-                <Login />
+                <RegisterBox />
+                <LoginBox />
             </div>
             )
     }
 })
 
-const Login = React.createClass ({
+const RegisterBox = React.createClass ({
+    _handleRegister: function(evt) {
+        evt.preventDefault()
+        ACTIONS.registerUser(evt.currentTarget.email.value,evt.currentTarget.password.value)
+    },
+
     render: function() {
         return (
-            <div className = 'loginForm'>
+            <div className = 'loginForm register'>
                 <form className = 'signIn' id = 'register'>
                     <h3>Register</h3>
                     <input type = 'text' name = 'registerName' placeholder = 'username' />
                     <input type = 'password' name = 'registerPassword' placeholder = 'password' />
                     <button type = 'submit'>Submit</button>
                 </form>
+                </div>
+                )
+    }
+})
+
+const LoginBox = React.createClass ({
+    _handleLogin: function(evt) {
+        evt.preventDefault()
+        ACTIONS.logUserIn(evt.target.email.value,evt.target.password.value)
+    },
+
+    render: function(){
+        return (
+            <div className = 'loginForm login'>
                 <form className = 'signIn' id = 'login'>
                     <h3>Login</h3>
                     <input type = 'text' name = 'loginName' placeholder = 'username' />
